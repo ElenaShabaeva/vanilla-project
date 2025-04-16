b148c8b25d26218f0499ade86c78831008b9add7
ElenaShabaeva <shabaevalenatomsk@gmail.com>
2025-04-16 14:21:06 +0700

    Изменения в проекте

--- src/js/select.js
+++ src/js/select.js
@@ -1,35 +1,38 @@
-export function chooseSelect(){
-  const select = document.querySelector("#js-select");
-  const selectMenu = document.querySelector("#js-select-menu");
-  const selectTitle = document.querySelector(".select__title");
-  const selectItems = document.querySelectorAll(".check");
-  const selectCaption = document.querySelector(".select__caption");
-  
-  select.addEventListener("click", () => {
-    select.classList.toggle("select__top--active");
-    
-    selectMenu.classList.toggle("select__menu--active")
-    selectCaption.classList.toggle("select__caption--none");
-  })
+export function chooseSelect() {
+    const select = document.querySelector("#js-select");
+    const selectMenu = document.querySelector("#js-select-menu");
+    const selectTitle = document.querySelector(".select__title");
+    const selectItems = document.querySelectorAll(".check");
+    const selectCaption = document.querySelector(".select__caption");
 
-  selectItems.forEach(item => {
-    item.addEventListener("change", () => {
-      item.classList.toggle("item");
+    select.addEventListener("click", () => {
+        select.classList.toggle("select__top--active");
 
-      let checked = document.querySelectorAll(".check input[type='checkbox']:checked");
-      
-      if(checked && checked.length > 0){
-            if(checked.length == 1){
-              const itemText = item.querySelector(".check__input");
-              selectTitle.textContent = itemText.value;
+        selectMenu.classList.toggle("select__menu--active");
+        selectCaption.classList.toggle("select__caption--none");
+    });
+
+    selectItems.forEach((item) => {
+        item.addEventListener("change", () => {
+            item.classList.toggle("item");
+
+            let checked = document.querySelectorAll(
+                ".check input[type='checkbox']:checked"
+            );
+
+            if (checked && checked.length > 0) {
+                if (checked.length == 1) {
+                    const itemText = item.querySelector(".check__input");
+                    selectTitle.textContent = itemText.value;
+                } else if (checked.length > 1 && checked.length < 5) {
+                    selectTitle.textContent = `Выбрано ${checked.length} варианта`;
+                } else if (checked.length == selectItems.length) {
+                    selectTitle.textContent = `Выбраны все варианты`;
+                }
             }
-            else if (checked.length > 1 && checked.length < 5){
-              selectTitle.textContent = `Выбрано ${checked.length} варианта`
+            else{
+              selectTitle.textContent = `Выберите из списка`
             }
-            else if(checked.length == selectItems.length){
-              selectTitle.textContent = `Выбраны все варианты`
-            }
-          }
-    })
-  })
+        });
+    });
 }
